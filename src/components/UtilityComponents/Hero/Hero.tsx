186010b5576a25509b9478035db4fb8280d4c863
Shubham Kushwaha <eric.liclair@gmail.com>
2021-04-28 21:36:13 +0530

    adding the search button

---
+++ src/components/UtilityComponents/Hero/Hero.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import SearchBar from "material-ui-search-bar";
+import "./Hero.scss";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="container">
+        <h1>CoviSource</h1>
+      </div>
+      <div className="container">
+        <div className="search">
+          <SearchBar />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
