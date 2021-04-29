dabd45d8209c3f55ac0902a86eb7254d4459c7e4
Shubham Kushwaha <eric.liclair@gmail.com>
2021-04-30 03:13:26 +0530

    resource card availability + sass scripts

--- src/CoviSource/Components/UtilityComponents/ResourcesCardSection/CardView.js
+++ src/CoviSource/Components/UtilityComponents/ResourcesCardSection/CardView.js
@@ -20,6 +20,15 @@ const status = {
   UNUPDATED: "warning",
 };
 
+// const hoverColors = {
+//   AVAILABLE:
+//     "linear-gradient(90deg, rgba(102, 187, 106, 0.1) 0%, rgba(56, 142, 60, 0.1) 100%)",
+//   UNAVAILABLE:
+//     "linear-gradient(90deg, rgba(255, 167, 38, 0.1) 0%, rgba(245, 124, 0, 0.1) 100%)",
+//   UNUPDATED:
+//     "linear-gradient(90deg, rgba(239, 83, 80, 0.1) 0%, rgba(211, 47, 47, 0.1) 100%)",
+// };
+
 CardView.propTypes = {
   data: propTypes.arrayOf(Object),
 };
@@ -30,6 +39,14 @@ export default function CardView(props) {
     setData("");
   }
   const classes = useStyles();
+
+  //   const hoverFunction = function (e) {
+  //     e.target.style.backgroundColor = "rgba(211, 47, 47, 0.1)";
+  //   };
+
+  //   const hoverRemove = function (e) {
+  //     e.target.style.background = "#ffffff";
+  //   };
 
   useEffect(() => {}, []);
   return (
@@ -55,9 +72,30 @@ export default function CardView(props) {
                           <ul className="cardViewList">
                             {details.resources.map((item, id) => {
                               return (
-                                <li key={id}>
-                                  <div className="data">{item.resource}</div>
-                                  <div className="info">{item.updated}</div>
+                                <li
+                                  key={id}
+                                  className={status[details["availability"]]}
+                                >
+                                  <div className="data">
+                                    <div className="dataResource">
+                                      <h6>{item.resource}</h6>
+                                    </div>
+                                    <div className="dataQuantity">
+                                      <p>
+                                        <span>{item.quantity} </span>available
+                                      </p>
+                                    </div>
+                                  </div>
+                                  <div className="info">
+                                    <div className="updateTag">
+                                      <h6>updated</h6>
+                                    </div>
+                                    <div className="updateInfo">
+                                      <p>
+                                        <span>{item.updated} </span>, 2021
+                                      </p>
+                                    </div>
+                                  </div>
                                 </li>
                               );
                             })}
