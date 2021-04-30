c94949a0094d797e0794e8d7c357c75c4c68b642
Shubham Kushwaha <65639964+EricLiclair@users.noreply.github.com>
2021-04-30 15:01:53 +0530

    Home page (#7)
    * removing srcc
    
    * resource cards
    
    * resource card availability + sass scripts
    
    * cards details and resourceType selection

--- src/CoviSource/Components/UtilityComponents/ResourcesCardSection/CardView.js
+++ src/CoviSource/Components/UtilityComponents/ResourcesCardSection/CardView.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
+import propTypes from "prop-types";
 
 // @material-ui/icons
 
@@ -8,136 +9,192 @@ import { makeStyles } from "@material-ui/core/styles";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import CustomTabs from "components/CustomTabs/CustomTabs.js";
+import Button from "components/CustomButtons/Button";
 
 import styles from "assets/jss/material-kit-react/views/componentsSections/basicsStyle.js";
-
+import "./CardView.scss";
+import { isMobile } from "CoviSource/UtilityFunctions";
 const useStyles = makeStyles(styles);
 
-export default function CardView() {
+const status = {
+  AVAILABLE: "success",
+  UNAVAILABLE: "danger",
+  UNUPDATED: "warning",
+};
+
+// const resourceTypes = {};
+
+CardView.propTypes = {
+  data: propTypes.arrayOf(Object),
+};
+
+export default function CardView(props) {
+  const [data, setData] = useState(props.data);
+  const [resourceType, setResourceType] = useState("ALL"); // oneOf["ALL", "AVAILABLE", "UNUPDATED", "UNAVAIBALE"]
+  if (data === null) {
+    setData("");
+  }
   const classes = useStyles();
+
+  const selectResourceType = function (e) {
+    setResourceType(e.target.innerText);
+  };
+
+  useEffect(() => {}, [resourceType]);
   return (
     <div className={classes.sections}>
       <div className={classes.container}>
         <div className={classes.title}>
           <h2>Resources</h2>
+          <div className={classes.space30} />
+          <Button
+            onClick={selectResourceType}
+            size={isMobile() ? "sm" : "md"}
+            color="primary"
+          >
+            All
+          </Button>
+          <Button
+            onClick={selectResourceType}
+            size={isMobile() ? "sm" : "md"}
+            color="success"
+          >
+            Available
+          </Button>
+          <Button
+            onClick={selectResourceType}
+            size={isMobile() ? "sm" : "md"}
+            color="warning"
+          >
+            unupdated
+          </Button>
+          <Button
+            onClick={selectResourceType}
+            size={isMobile() ? "sm" : "md"}
+            color="danger"
+          >
+            Unavailable
+          </Button>
           <div className={classes.space70} />
         </div>
         <div id="nav-tabs">
           <GridContainer>
-            <GridItem xs={12} sm={12} md={6}>
-              <CustomTabs
-                headerColor="success"
-                tabs={[
-                  {
-                    tabName: "AVAILABILITY",
-                    tabContent: (
-                      <p className={classes.textCenter}>
-                        I think that’s a responsibility that I have, to push
-                        possibilities, to show people, this is the level that
-                        things could be at. So when you get something that has
-                        the name Kanye West on it, it’s supposed to be pushing
-                        the furthest possibilities. I will be the leader of a
-                        company that ends up being worth billions of dollars,
-                        because I got the answers. I understand culture. I am
-                        the nucleus.
-                      </p>
-                    ),
-                  },
-                  {
-                    tabName: "DETAILS",
-                    // tabIcon: Share,
-                    tabContent: (
-                      <p className={classes.textCenter}>
-                        I think that’s a responsibility that I have, to push
-                        possibilities, to show people, this is the level that
-                        things could be at. I will be the leader of a company
-                        that ends up being worth billions of dollars, because I
-                        got the answers. I understand culture. I am the nucleus.
-                        I think that’s a responsibility that I have, to push
-                        possibilities, to show people, this is the level that
-                        things could be at.
-                      </p>
-                    ),
-                  },
-                ]}
-              />
-            </GridItem>
-            <GridItem xs={12} sm={12} md={6}>
-              <CustomTabs
-                headerColor="warning"
-                tabs={[
-                  {
-                    tabName: "AVAILABILITY",
-                    tabContent: (
-                      <p className={classes.textCenter}>
-                        I think that’s a responsibility that I have, to push
-                        possibilities, to show people, this is the level that
-                        things could be at. So when you get something that has
-                        the name Kanye West on it, it’s supposed to be pushing
-                        the furthest possibilities. I will be the leader of a
-                        company that ends up being worth billions of dollars,
-                        because I got the answers. I understand culture. I am
-                        the nucleus.
-                      </p>
-                    ),
-                  },
-                  {
-                    tabName: "DETAILS",
-                    // tabIcon: Share,
-                    tabContent: (
-                      <p className={classes.textCenter}>
-                        I think that’s a responsibility that I have, to push
-                        possibilities, to show people, this is the level that
-                        things could be at. I will be the leader of a company
-                        that ends up being worth billions of dollars, because I
-                        got the answers. I understand culture. I am the nucleus.
-                        I think that’s a responsibility that I have, to push
-                        possibilities, to show people, this is the level that
-                        things could be at.
-                      </p>
-                    ),
-                  },
-                ]}
-              />
-            </GridItem>
-            <GridItem xs={12} sm={12} md={6}>
-              <CustomTabs
-                headerColor="danger"
-                tabs={[
-                  {
-                    tabName: "AVAILABILITY",
-                    tabContent: (
-                      <p className={classes.textCenter}>
-                        I think that’s a responsibility that I have, to push
-                        possibilities, to show people, this is the level that
-                        things could be at. So when you get something that has
-                        the name Kanye West on it, it’s supposed to be pushing
-                        the furthest possibilities. I will be the leader of a
-                        company that ends up being worth billions of dollars,
-                        because I got the answers. I understand culture. I am
-                        the nucleus.
-                      </p>
-                    ),
-                  },
-                  {
-                    tabName: "DETAILS",
-                    // tabIcon: Share,
-                    tabContent: (
-                      <p className={classes.textCenter}>
-                        I think that’s a responsibility that I have, to push
-                        possibilities, to show people, this is the level that
-                        things could be at. I will be the leader of a company
-                        that ends up being worth billions of dollars, because I
-                        got the answers. I understand culture. I am the nucleus.
-                        I think that’s a responsibility that I have, to push
-                        possibilities, to show people, this is the level that
-                        things could be at.
-                      </p>
-                    ),
-                  },
-                ]}
-              />
-            </GridItem>
+            {data.map((details, key) => {
+              if (
+                resourceType === "ALL" ||
+                resourceType === details["availability"]
+              ) {
+                return (
+                  <GridItem key={key} xs={12} sm={12} md={6}>
+                    <CustomTabs
+                      headerColor={status[details["availability"]]}
+                      tabs={[
+                        {
+                          tabName: "AVAILABILITY",
+                          tabContent: (
+                            <ul className="cardViewList">
+                              {details.resources.map((item, id) => {
+                                return (
+                                  <li
+                                    key={id}
+                                    className={status[details["availability"]]}
+                                  >
+                                    <div className="data">
+                                      <div className="dataResource">
+                                        <h6>{item.resource}</h6>
+                                      </div>
+                                      <div className="dataQuantity">
+                                        <p>
+                                          <span>{item.quantity} </span>available
+                                        </p>
+                                      </div>
+                                    </div>
+                                    <div className="info">
+                                      <div className="updateTag">
+                                        <h6>updated</h6>
+                                      </div>
+                                      <div className="updateInfo">
+                                        <p>
+                                          <span>{item.updated} </span>, 2021
+                                        </p>
+                                      </div>
+                                    </div>
+                                  </li>
+                                );
+                              })}
+                            </ul>
+                          ),
+                        },
+                        {
+                          tabName: "DETAILS",
+                          // tabIcon: Share,
+                          tabContent: (
+                            <ul className="cardViewList">
+                              <li
+                                className={
+                                  status[details["availability"]] + " detail"
+                                }
+                              >
+                                <div className="details">
+                                  <div className="detailsInfo">
+                                    <h6>ADDRESS</h6>
+                                  </div>
+                                  <div className="detailsData">
+                                    <p>
+                                      <span>{details.address}</span>
+                                    </p>
+                                  </div>
+                                </div>
+                              </li>
+                              <li className={status[details["availability"]]}>
+                                <div className="data">
+                                  <div className="dataResource">
+                                    <h6>contact</h6>
+                                  </div>
+                                  <div className="dataQuantity">
+                                    <p>
+                                      <span>{details.contactPersonName}</span>
+                                    </p>
+                                  </div>
+                                </div>
+                                <div className="info">
+                                  <div className="updateTag">
+                                    <h6>phone</h6>
+                                  </div>
+                                  <div className="updateInfo">
+                                    <p>
+                                      <span>{details.contactNumber}</span>
+                                    </p>
+                                  </div>
+                                </div>
+                              </li>
+                              <li
+                                className={
+                                  status[details["availability"]] + " detail"
+                                }
+                              >
+                                <div className="details">
+                                  <div className="detailsInfo">
+                                    <h6>{details.serviceName}</h6>
+                                  </div>
+                                  <div className="detailsData">
+                                    <p>
+                                      <span>
+                                        {details.resourceProviderName}
+                                      </span>
+                                    </p>
+                                  </div>
+                                </div>
+                              </li>
+                            </ul>
+                          ),
+                        },
+                      ]}
+                    />
+                  </GridItem>
+                );
+              }
+            })}
           </GridContainer>
         </div>
       </div>
