417fbeba6a15c5b650dfb390b92b01400fdd625c
Yash Pandey (YP) <yash.btech.cs19@iiitranchi.ac.in>
2021-05-10 00:21:36 +0530

    Fixed login

--- client/src/CoviSource/graphql/queries/Provider/FindProvider.js
+++ client/src/CoviSource/graphql/queries/Provider/FindProvider.js
@@ -3,7 +3,7 @@ import { gql } from "@apollo/client";
 export const FIND_PROVIDER = gql`
   query FindProvider($slug: String!) {
     findProviderbySlug(slug: $slug) {
-      id
+      _id
       providerName
       address
       location {
